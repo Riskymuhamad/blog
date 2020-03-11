8e3ed5a678da18b9b9ad627007dc8d175705fb69
riskymuhamad <riskymuhamad11@gmail.com>
2020-03-11 21:38:54 +0700

    user info terbaru

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
+import risky from '../../content/images/profile.jpg'
 
 export default class UserInfo extends Component {
   render() {
     return (
       <aside className="note">
         <div className="container note-container">
-        
+          <div className="flex-author">
+              
+              <div className="flex-avatar">
+           <center>
+              <img className="avatar" src={risky} alt="Risky Muhamad" />
+          </center>
             <div>
+              <center>
               <p>
-                Terima kasih telah membaca di situs ini
+                  Terima kasih telah berkunjung dan membaca di situs saya, semoga dapat memberikan manfaat.
               </p>
-          
+              <p>
+                  ~Risky Muhamad~
+              </p>
+            </center>
+            </div>
+            </div>
           </div>
         </div>
       </aside>
